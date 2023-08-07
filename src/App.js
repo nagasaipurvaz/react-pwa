4752141ef808712cbf5b5a0a3b1efaec218901c0
nagasaipurvaz <68222105+nagasaipurvaz@users.noreply.github.com>
2023-08-07 21:27:16 +0530

    added the main functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Forms from './form';
+import axios from 'axios';
+import { useState,useEffect } from 'react';
+import './App.css'
 
-function App() {
+const App = () => {
+  const [connectionCount, setConnectionCount] = useState(0);
+  useEffect(() => {
+    logFrontendConnection();
+  }, []);
+
+  const logFrontendConnection = () => {
+    axios.post('http://localhost:5000/api/connection', { connectionCount: 1 })
+      .then((response) => {
+        console.log(response.data.message);
+        setConnectionCount((prevCount) => prevCount + 1);
+      })
+      .catch((error) => {
+        console.error('Error logging frontend connection:', error);
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className="app">
+      <h1>Glass Morphism Form</h1>
+      <h1>Frontend Connection Count: {connectionCount}</h1>
+      <Forms/>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/form.js
@@ -0,0 +1,58 @@
+import React, { useState} from 'react';
+import './form.css';
+import axios from 'axios';
+
+const Forms = () => {
+    const [inputString, setInputString] = useState('');
+  const [savedInput, setSavedInput] = useState(null);
+
+
+
+
+
+  const handleInputChange = (event) => {
+    setInputString(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (inputString.trim() === '') {
+      return;
+    }
+
+    axios.post('http://localhost:5000/api/input', { inputString })
+      .then((response) => {
+        setSavedInput(response.data);
+        setInputString('');
+      })
+      .catch((error) => console.error('Error saving input:', error));
+  };
+
+  return (
+    <div>
+    <form className="glass-morphism-form" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={inputString}
+        onChange={handleInputChange}
+        className="glass-morphism-input"
+        placeholder="Enter your text..."
+      />
+      <button type="submit" className="glass-morphism-button">
+        Submit
+      </button>
+    </form>
+
+    {savedInput && (
+        <div>
+          <h2>Input Saved:</h2>
+          <p>{savedInput.inputString}</p>
+          <p>Created At: {new Date(savedInput.createdAt).toLocaleString()}</p>
+        </div>
+      )}
+
+      </div>
+  );
+};
+
+export default Forms;
